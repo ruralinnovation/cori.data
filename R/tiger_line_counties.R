04c937e87c2c66b8a6702dc5842c8debce00861b
John Hall <john.hall@ruralinnovation.us>
2025-10-30 12:56:58 -0400

    cartographic county boundaries are available

--- R/tiger_line_counties.R
+++ R/tiger_line_counties.R
@@ -6,7 +6,7 @@ library(sf)
 
 #' Function to load Census County boundaries from S3 (or from local disk, if cached as .RDS)
 #'
-#' @param tiger_year integer, year of data release
+#' @param year character, year of data release; prefix with "cb_" for cartographic boundaries
 #'
 #' @return return Census County boundaries as sf data.frame
 #'
@@ -15,10 +15,10 @@ library(sf)
 #' @examples
 #'
 #' \dontrun{
-#'  counties <- tiger_line_counties(2024)
+#'  counties <- tiger_line_counties("2024") # Returns full tiger line 2024 county boundary geomtry
 #' }
 #'
-tiger_line_counties <- function (tiger_year = 2024) {
+tiger_line_counties <- function (year = "cb_2024") {
 
     # setup data dir
     data_dir <- paste0(here::here(), "/data")
@@ -28,6 +28,16 @@ tiger_line_counties <- function (tiger_year = 2024) {
 
     data_prefix <- "tiger/line/counties"
     s3_bucket_name <- "cori.data.census"
+      
+    if (startsWith(year, "20")) {
+      tiger_year <- paste0("tl_")
+    } else if (startsWith(year, "tl_")) {
+      tiger_year <- year
+    } else if (startsWith(year, "cb_")) {
+      tiger_year <- year
+    } else {
+      stop("tiger_line_counties expects `year` argument as \"YYYY\", \"tl_YYYY\", or \"cb_YYYY\" (for cartographic boundaries)")
+    }
 
     if (!file.exists(paste0(data_dir, "/counties_", tiger_year, ".rds"))) {
 
--- data-raw/write_states_source_s3.R
+++ data-raw/write_states_source_s3.R
@@ -2,7 +2,7 @@ library(cori.db)
 library(dplyr)
 
 # setup data dir
-data_dir <- paste0(here::here(), "/data/")
+data_dir <- paste0(here::here(), "/inst/ext_data/")
 if (! dir.exists(data_dir)) dir.create(data_dir)
 
 ### WRITE ------------------------------------------------------------------------------------------------------------
@@ -10,21 +10,21 @@ if (! dir.exists(data_dir)) dir.create(data_dir)
 data_prefix <- "tiger/line/states"
 s3_bucket_name <- "cori.data.census"
 
-cori.db::put_s3_objects_recursive(s3_bucket_name, data_prefix, paste0("data/source/", data_prefix))
+cori.db::put_s3_objects_recursive(s3_bucket_name, data_prefix, paste0("inst/ext_data/source/", data_prefix))
 
 
 #### TEST ------------------------------------------------------------------------------------------------------------
 
-dir.create(paste0(data_dir, "/data/test"), showWarnings = FALSE)
+dir.create(paste0(data_dir, "/inst/ext_data/test"), showWarnings = FALSE)
 
-cori.db::get_s3_object(s3_bucket_name, paste0(data_prefix, "/tl_2024_us_state.zip"), "data/test/tl_2024_us_state.zip")
+cori.db::get_s3_object(s3_bucket_name, paste0(data_prefix, "/tl_2024_us_state.zip"), "inst/ext_data/test/tl_2024_us_state.zip")
 
 data_s3_files <- (
     cori.db::list_s3_objects(bucket_name = s3_bucket_name) |> dplyr::filter(grepl(data_prefix, key))
 )$key
 
-dir.create(paste0("data/test/", data_prefix), recursive = TRUE, showWarnings = FALSE)
+dir.create(paste0("inst/ext_data/test/", data_prefix), recursive = TRUE, showWarnings = FALSE)
 
 data_files <- lapply(data_s3_files,function (s3_key_path) {
-    cori.db::get_s3_object(s3_bucket_name, s3_key_path, paste0("data/test/", data_prefix, "/", basename(s3_key_path)))
+    cori.db::get_s3_object(s3_bucket_name, s3_key_path, paste0("inst/ext_data/test/", basename(s3_key_path)))
 })
